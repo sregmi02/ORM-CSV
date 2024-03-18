1c6e591c7d4ffef7834c8a1273d004e8a74001f4
Subrat Regmi <regmisubrat02@gmail.com>
2024-03-18 14:02:45 +0545

    ORM-CSV

--- orm-csv-saver.py
+++ orm-csv-saver.py
@@ -1,85 +1,38 @@
 import os
-
+#create a parent class CSV_Saver
 class CSV_Saver:
-
+    #static method for creating a file
     @staticmethod
-    def create(file_name):
+    def create(file_name, col1, col2, col3):
         #create a file if it doesn't exist
         if not os.path.exists(file_name):
             with open(file_name, 'w') as file:
-                cols = input("Enter Column Names(column separated): ").split(',')
-                for i in cols:
-                    file.write(f"{i},")
-                file.write("\n")
+                file.write(f"{col1},{col2},{col3}\n")
                 print("File Created")
         else:
             print('File Found')
 
-
-    def read(self):
+    #class method for reading a file
+    @classmethod
+    def read(cls):
         #read values from file
-        f = open(self.file_name, 'r')
+        f = open(cls.file_name, 'r')
         print(f.read())
 
-    def write(self):
-        pass
-
-
-    def update(self):
-        pass
-        
-    def delete(self):
-        #delete records
-        deleted = False
-        id = input("Enter Record ID: ")
-
-        with open(self.file_name, 'r') as file:
-            lines = file.readlines()
-
-        with open(self.file_name, 'w') as file:
-            for line in lines:
-                if line.strip().split(',')[0] == id:
-                    deleted = True
-                    continue
-                file.write(line)
-            if not deleted:
-                print("Record not found")
-            else:
-                print("Record Deleted")
-
-
-class CSV_Operation(CSV_Saver):
-    def __init__(self, file_name):
-        self.file_name = file_name
-        super().create(file_name)
-
-    def write(self):
-        #add records to a file
-        with open(self.file_name, 'r') as file:
-            lines = file.readlines()
-            rows = input(f"Enter Values for {lines[0].split()} (comma separated): ").split(',')
-            for line in file:
-                if line.strip().split(',')[0] == rows[0].strip():
-                    print("ID already exists. Cannot add value.")
-                    return
-        with open(self.file_name, 'a') as file:    
-            for i in rows:
-                file.write(f"{i},")
-            file.write("\n")
-            print("Record Added")
-
-    def update(self):
+    #classmethod for updating existing records
+    @classmethod
+    def update(cls):
         found = False
         id = input("Enter Record ID to Update: ")
 
-        with open(self.file_name, 'r') as file:
+        with open(cls.file_name, 'r') as file:
             lines = file.readlines()
             for line in lines:
                 if line.strip().split(',')[0] == id:
                     found = True
         if found == True:
             row = input(f"Enter Values (comma separated): ").split(',')
-            with open(self.file_name, 'w') as file:
+            with open(cls.file_name, 'w') as file:
                 for line in lines:
                     if line.strip().split(',')[0] == id:
                         file.write(f"{id},")
@@ -92,26 +45,76 @@ class CSV_Operation(CSV_Saver):
         else:
             print("Record Not Found")
 
+    #classmethod for deleting records
+    @classmethod   
+    def delete(cls):
+        #delete records
+        deleted = False
+        id = input("Enter Record ID: ")
 
+        with open(cls.file_name, 'r') as file:
+            lines = file.readlines()
+
+        with open(cls.file_name, 'w') as file:
+            for line in lines:
+                if line.strip().split(',')[0] == id:
+                    deleted = True
+                    continue
+                file.write(line)
+            if not deleted:
+                print("Record not found")
+            else:
+                print("Record Deleted")
+
+#child class inherits csv_saver class
+class CSV_Operation(CSV_Saver):
+
+    def __init__(self, file_name, col1, col2, col3):
+        CSV_Operation.file_name = file_name
+        CSV_Operation.col1 = col1
+        CSV_Operation.col2 = col2
+        CSV_Operation.col3 = col3
+        super().create(file_name, col1, col2, col3)
+
+    #classmethod for adding new records.
+    @classmethod
+    def write(cls):
+        with open(cls.file_name, 'r') as file:
+            lines = file.readlines()
+            rows = input(f"Enter Values for {lines[0].split()} (comma separated): ").split(',')
+            for line in lines:
+                if line.strip().split(',')[0] == rows[0].strip():
+                    print("ID already exists. Cannot add value.")
+                    return
+        with open(cls.file_name, 'a') as file:    
+            for i in rows:
+                file.write(f"{i},")
+            file.write("\n")
+            print("Record Added")
+
+
+#main
 file_name =input("Enter file name: ")
-csv_op = CSV_Operation(file_name)
+csv_op = CSV_Operation(file_name, "ID", "Name", "Price")
+
 while True:
     print("Enter an Operation")
     print("1. Add Record\n2. Delete Record\n3. Update Record\n4. Read Records\n5. Exit")
     choice = int(input())
     if choice == 1:
-        #write
-        csv_op.write()
+        #write operation accesses the method from its own class
+        CSV_Operation.write()
     elif choice == 2:
-        #delete
-        csv_op.delete()
+        #delete operation accesses the method from parent class
+        CSV_Operation.delete()
     elif choice == 3:
-        #update
-        csv_op.update()
+        #update operation accesses the method from parent class
+        CSV_Operation.update()
     elif choice == 4:
-        #read
-        csv_op.read()
+        #read operation accesses the method from parent classs
+        CSV_Operation.read()
     elif choice == 5:
+        #exit control
         exit(-1)
     else:
         print("Wrong Input")
